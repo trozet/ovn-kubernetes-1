3661e35a93db4d1d9d7e21d62b06e294dc4234d7
Tim Rozet <trozet@redhat.com>
2021-03-05 17:48:26 -0500

    Stop using "GR_" everywhere
    Makes util functions to handle determing GR and worker switch names from
    constants.
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- go-controller/pkg/ovn/controller/services/utils.go
+++ go-controller/pkg/ovn/controller/services/utils.go
@@ -147,7 +147,7 @@ func deleteVIPsFromOVN(vips sets.String, st *serviceTracker, name, namespace, cl
 				// TODO: why continue? should we error and requeue and retry?
 				continue
 			}
-			workerNode := strings.TrimPrefix(gatewayRouter, "GR_")
+			workerNode := util.GetWorkerFromGatewayRouter(gatewayRouter)
 			workerLB, err := loadbalancer.GetWorkerLoadBalancer(workerNode, proto)
 			if err != nil {
 				klog.Errorf("Worker switch %s does not have load balancer (%v)", workerNode, err)
@@ -208,7 +208,7 @@ func createPerNodeVIPs(svcIPs []string, protocol v1.Protocol, sourcePort int32, 
 		}
 
 		if config.Gateway.Mode == config.GatewayModeShared {
-			workerNode := strings.TrimPrefix(gatewayRouter, "GR_")
+			workerNode := util.GetWorkerFromGatewayRouter(gatewayRouter)
 			workerLB, err := loadbalancer.GetWorkerLoadBalancer(workerNode, protocol)
 			if err != nil {
 				klog.Errorf("Worker switch %s does not have load balancer (%v)", workerNode, err)
@@ -251,7 +251,7 @@ func deleteNodeVIPs(svcIPs []string, protocol v1.Protocol, sourcePort int32) err
 		}
 		loadBalancers = append(loadBalancers, gatewayLB)
 		if config.Gateway.Mode == config.GatewayModeShared {
-			workerNode := strings.TrimPrefix(gatewayRouter, "GR_")
+			workerNode := util.GetWorkerFromGatewayRouter(gatewayRouter)
 			workerLB, err := loadbalancer.GetWorkerLoadBalancer(workerNode, protocol)
 			if err != nil {
 				klog.Errorf("Worker switch %s does not have load balancer (%v)", workerNode, err)
